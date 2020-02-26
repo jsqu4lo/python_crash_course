d28fe2fa6b7d62bdedf6df07343bc848e17a9152
toastx01 <59560121+toastx01@users.noreply.github.com>
2020-02-26 16:29:30 +0700

    Update sum_divisors.py

--- sum_divisors.py
+++ sum_divisors.py
@@ -2,7 +2,7 @@ def sum_divisors(n):
   m = 1
   sum = 0
   # Return the sum of all divisors of n, not including n
-  while m != n:
+  while m < n:
     if n%m == 0:
       sum = sum + m
     m += 1
@@ -11,6 +11,3 @@ def sum_divisors(n):
 print(sum_divisors(6)) # Should be 6 (sum of 1+2+3)
 print(sum_divisors(12)) # Should be 16 (sum of 1+2+3+4+6)
 
-#Incorrect
-#Timed out after 5
-#what???
