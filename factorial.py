dbc3977c39eaa9cdcea3b7a36e52cf95b7bfd898
toastx01 <59560121+toastx01@users.noreply.github.com>
2020-03-04 14:33:32 +0700

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -6,3 +6,15 @@ def factorial(n):
 
 print(factorial(4)) # should return 24
 print(factorial(5)) # should return 120
+
+
+print("###############################################")
+
+def factorial(n):
+    result = 1
+    for x in range(1,n):
+        result = x * result
+    return result
+
+for n in range(1,10):
+    print(n, factorial(n+1))
